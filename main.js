2d81db57b694969e1d67da516c7eeb697122c883
moka0690 <madradz39@gmail.com>
2024-02-14 21:54:42 +0100

    Solving the 1-2-3-4 exercise

---
+++ main.js
@@ -0,0 +1,13 @@
+document. write ("<h1>Elzero</h1> ");
+console. log("%c Elzero %c web %c school","color:red; font-saiz:40px" ,"color:Green; font-saiz:40px; font-weight:bold","color:wait; font-saiz:40px; font-weight:bold; background-color:blue");
+console.group("group1")
+console.error("masseg1")
+console.error("masseg2")
+console.group("child group")
+console.error("masseg1")
+console.error("masseg1")
+console.groupEnd()
+console.groupEnd()
+console.group("group1")
+console.error("masseg1")
+console.error("masseg2")
